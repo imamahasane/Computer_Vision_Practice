5368cfd822cba7bc49fa1ede32814f24d4c00d06
Md Imam Ahasan <emamahasane@gmail.com>
2021-12-10 07:39:27 +0600

    Add files via upload

--- OpenCV_with_Python/transformations.py
+++ OpenCV_with_Python/transformations.py
@@ -41,6 +41,10 @@ cv.imshow("Rotated Rotated", rotated_rotated)
 resized = cv.resize(img, (500, 500), interpolation= cv.INTER_CUBIC)
 cv.imshow("Resized", resized)
 
+# Flipping
+flip = cv.flip(img, 0)
+cv.imshow
+
 
 cv.waitKey(0) 
 
